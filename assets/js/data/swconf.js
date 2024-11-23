67d1459b2c42aa6e617c3200495f04c519c145c8
Hyunwoo <hw1115.lee@gmail.com>
2024-11-23 20:50:35 +0900

    edit config&article

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732358968',resources: [
+    cacheName: 'chirpy-1732362594',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
