135fc0edfe93392ff0869eabca536ae2311da172
Hyunwoo <hw1115.lee@gmail.com>
2024-11-24 13:34:24 +0900

    edit content-max-width to 3000px & upload new post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732379964',resources: [
+    cacheName: 'chirpy-1732422786',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
