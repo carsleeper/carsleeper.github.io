dd49b99d135f44982e3abd0ce16c54e9831bc890
Hyunwoo <hw1115.lee@gmail.com>
2024-11-27 20:32:40 +0900

    new post:IEEE 10-1

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732537281',resources: [
+    cacheName: 'chirpy-1732707127',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
