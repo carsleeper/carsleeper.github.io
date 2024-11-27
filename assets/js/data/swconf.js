cac64f50118a6bc9016feab1a75f53502fee8d09
Hyunwoo <hw1115.lee@gmail.com>
2024-11-27 20:38:33 +0900

    create category

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732707127',resources: [
+    cacheName: 'chirpy-1732707487',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
